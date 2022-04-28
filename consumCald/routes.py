690ce9090fa657858bdc7a28392ea5ffb6186718
araduflorin <araduflorin@yahoo.com>
2022-04-28 14:44:12 +0300

    init2

--- consumCald/routes.py
+++ consumCald/routes.py
@@ -1,7 +1,13 @@
-from consumCald import app
-from flask import render_template
+#from consumCald import app
+import sqlite3
+from flask import render_template, Flask, redirect, request, g
+
+
+app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
 def home_page():
     return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from consumCald import app
+#from consumCald import app
+
 
 #Checks if the run.py file has executed directly and not imported
-if __name__ == '__main__':
-    app.run(debug=True)
